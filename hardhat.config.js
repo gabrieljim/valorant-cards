784ff3b7665aff5b59acd4022e46508adc51a8e5
Gabriel <gabrieljim641@gmail.com>
2021-11-02 14:09:16 -0400

    Added fund-link hardhat task

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,3 +1,5 @@
+const { parseEther } = require("ethers/lib/utils");
+
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 require("@nomiclabs/hardhat-etherscan");
@@ -5,6 +7,54 @@ require("@nomiclabs/hardhat-etherscan");
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+task("fund-link", "Funds a contract with LINK")
+  .addParam("contract", "The address of the contract that requires LINK")
+  .addOptionalParam("linkAddress", "Set the LINK token address")
+  .setAction(async (taskArgs) => {
+    const linkAddress = "0x01BE23585060835E02B77ef475b0Cc51aA1e0709";
+    const contractAddr = taskArgs.contract;
+    const networkId = network.name;
+    console.log("Funding contract ", contractAddr, " on network ", networkId);
+    const LINK_TOKEN_ABI = [
+      {
+        inputs: [
+          { internalType: "address", name: "_to", type: "address" },
+          { internalType: "uint256", name: "_value", type: "uint256" },
+        ],
+        name: "transfer",
+        outputs: [{ internalType: "bool", name: "", type: "bool" }],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ];
+
+    //Fund with 0.1 LINK token
+    const amount = parseEther("0.1");
+
+    //Get signer information
+    const accounts = await hre.ethers.getSigners();
+    const signer = accounts[0];
+
+    //Create connection to LINK token contract and initiate the transfer
+    const linkTokenContract = new ethers.Contract(
+      linkAddress,
+      LINK_TOKEN_ABI,
+      signer
+    );
+
+    var result = await linkTokenContract
+      .transfer(contractAddr, amount)
+      .then(function (transaction) {
+        console.log(
+          "Contract ",
+          contractAddr,
+          " funded with 1 LINK. Transaction Hash: ",
+          transaction.hash
+        );
+      });
+  });
+
 module.exports = {
   solidity: "0.8.4",
   networks: {
