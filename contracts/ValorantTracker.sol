3a4426c716d207771b75a51a4bb30026390233db
Gabriel <gabrieljim641@gmail.com>
2021-11-02 12:13:10 -0400

    Optimized initial minting

--- contracts/ValorantTracker.sol
+++ contracts/ValorantTracker.sol
@@ -10,43 +10,36 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract ValorantTracker is ERC1155, ChainlinkClient, Ownable {
     using Chainlink for Chainlink.Request;
 
-    uint8 public constant CLASSIC = 0;
-    uint8 public constant SHORTY = 1;
-    uint8 public constant FRENZY = 2;
-    uint8 public constant GHOST = 3;
-    uint8 public constant SHERIFF = 4;
-    uint8 public constant BUCKY = 5;
-    uint8 public constant STINGER = 6;
-    uint8 public constant MARSHAL = 7;
-    uint8 public constant ARES = 8;
-    uint8 public constant SPECTRE = 9;
-    uint8 public constant JUDGE = 10;
-    uint8 public constant BULLDOG = 11;
-    uint8 public constant GUARDIAN = 12;
-    uint8 public constant PHANTOM = 13;
-    uint8 public constant VANDAL = 14;
-    uint8 public constant ODIN = 15;
-    uint8 public constant OPERATOR = 16;
+    enum WEAPONS {
+        CLASSIC,
+        SHORTY,
+        FRENZY,
+        GHOST,
+        SHERIFF,
+        BUCKY,
+        STINGER,
+        MARSHAL,
+        ARES,
+        SPECTRE,
+        JUDGE,
+        BULLDOG,
+        GUARDIAN,
+        PHANTOM,
+        VANDAL,
+        ODIN,
+        OPERATOR
+    }
 
     constructor() ERC1155("") {
         setPublicChainlinkToken();
-        _mint(address(this), CLASSIC, 1, "");
-        _mint(address(this), SHORTY, 1, "");
-        _mint(address(this), FRENZY, 1, "");
-        _mint(address(this), GHOST, 1, "");
-        _mint(address(this), SHERIFF, 1, "");
-        _mint(address(this), BUCKY, 1, "");
-        _mint(address(this), STINGER, 1, "");
-        _mint(address(this), MARSHAL, 1, "");
-        _mint(address(this), ARES, 1, "");
-        _mint(address(this), SPECTRE, 1, "");
-        _mint(address(this), JUDGE, 1, "");
-        _mint(address(this), BULLDOG, 1, "");
-        _mint(address(this), GUARDIAN, 1, "");
-        _mint(address(this), PHANTOM, 1, "");
-        _mint(address(this), VANDAL, 1, "");
-        _mint(address(this), ODIN, 1, "");
-        _mint(address(this), OPERATOR, 1, "");
+
+        for (
+            uint8 i = uint8(WEAPONS.CLASSIC);
+            i < uint8(WEAPONS.OPERATOR);
+            i++
+        ) {
+            _mint(address(this), i, 1, "");
+        }
     }
 
     function uri(uint256 _tokenId)
